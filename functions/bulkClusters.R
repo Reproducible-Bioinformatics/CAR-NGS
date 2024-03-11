a1578e3b17732f564391d005f6f6ab34d5feaaa4
agatadonofrio <127829929+agatadonofrio@users.noreply.github.com>
2024-03-11 11:45:18 +0100

    Update bulkClusters.R

--- functions/bulkClusters.R
+++ functions/bulkClusters.R
@@ -2,15 +2,16 @@
 #' @title bulkClusters
 #' @description The present function create pseudo-bulk matrix from clustering.output file. The output are three files: _bulklog2, which is not normalized, _bulkColumn, which is z-scoere calculated over each column, _bulkRow, which is z-score calculated over each row
 #' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
-#' @param scratch.folder, a character string indicating the path of the scratch folder
+#' @param scratch.folderDOCKER, a character string indicating the path of the scratch folder inside the docker
+#' @param scratch.folderHOST, a character string indicating the path of the scratch folder inside the host. If not running from docker, this is the character string that indicates the path of the scratch.folder
 #' @param file, a character string indicating the path of the file, with file name and extension included
 #' @param cl, name and path of the file clustering.output previously generated from clustering algorithm from rcasc
 #' @param separator, matrix separator, ',', '\\t'
-#' @author Luca Alessandri, alessandri [dot] luca1991 [at] gmail [dot] com, University of Torino
+#' @author Luca Alessandri, Sebastian Bucatariu, Agata D'Onofrio
 #'
 #' @examples
 #' \dontrun{
-#'  bulkClusters(group="docker", scratch.folder="/home/user/scratch", 
+#'  bulkClusters(group="docker", scratch.folderDOCKER="/sharedFolder/scratch", scratch.folderHOST="/home/user/scratch"
 #'  file="/home/user/temp/setA.csv",separator=",",
 #'  cl="/home/user/temp/Results/setA/3/setA_clustering.output.csv")
 #'  
