087a8fe2e453a9d072fa79f89348212917f21f5e
Eliseo Martelli <me@eliseomartelli.it>
2024-07-26 14:09:44 +0200

    chore: move run_in_docker and is_running_in_docker & add documentation
    Addresses:
    GitHub issue(s): #2, #3, #5

--- R/dimensions.R
+++ R/dimensions.R
@@ -60,42 +60,6 @@ dimensions <- function(result_dir_path, input_file_path, separator) {
   )
 }
 
-run_in_docker <- function(image_name,
-                          volumes = list(),
-                          additional_arguments = c()) {
-  base_command <- "run --privileged=true --platform linux/amd64 --rm"
-  for (volume in volumes) {
-    volume[1] <- normalize_path(volume[1],
-      path_mappers = c(docker_mount_mapper)
-    )
-    base_command <- paste(base_command, "-v", paste(
-      volume[1],
-      volume[2],
-      sep = ":"
-    ))
-  }
-  base_command <- paste(base_command, image_name)
-  for (argument in additional_arguments) {
-    base_command <- paste(base_command, argument)
-  }
-  system2("docker", args = base_command, stdout = TRUE, stderr = TRUE)
-}
-
-#' Check if the script is running in a container.
-#'
-#' @returns A truthy value indicating the state.
-is_running_in_docker <- function() {
-  dockerenv_exists <- file.exists("/.dockerenv")
-  cgroup_exists <- file.exists("/proc/1/cgroup")
-  in_container_runtime <- FALSE
-  if (cgroup_exists) {
-    in_container_runtime <- any(
-      grepl("docker", readLines("/proc/1/cgroup", warn = FALSE))
-    )
-  }
-  return(dockerenv_exists || in_container_runtime)
-}
-
 #' Maps a path to host volumes.
 #'
 #' @param path a normalized, absolute path.
