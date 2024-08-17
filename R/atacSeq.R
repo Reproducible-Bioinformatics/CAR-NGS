ff491f8c349f8e847d29e58972595487765fad13
agatadonofrio <127829929+agatadonofrio@users.noreply.github.com>
2024-08-17 12:35:04 +0200

    feat(Atac-seq): add atacSeq function
    Adding the function with the proper documentation after testing it

---
+++ R/atacSeq.R
@@ -0,0 +1,61 @@
+#' Atac-seq
+#'
+#' This function is used to do a Assay for Transposase-Accessible Chromatin with high-throughput sequencing analysis,
+#' it returns quality control files for each fastq (FastQC reports),
+#' BAM files containing aligned reads in BAM format,
+#' index files (.bai) for each BAM file,
+#' Peak Calling results and BigWig files
+#'
+#' @param input_dir_path, a character string indicating the path of a directory containing the fastq files to be analyzed
+#' @param genome_dir_path, a character string indicating the path of a directory containing the fasta files of the genome to be analyzed
+#' @param nThreads, number of cores for parallelization
+#' @author Luca Alessandri, Agata D'Onofrio
+#'
+#' @examples
+#' \dontrun{
+#'  atacSeq(
+#'     input_dir_path="/the/input/path",
+#'     genome_dir_path="/the/genome/path"
+#'  )
+#' }
+#' @export
+atacSeq <- function(input_dir_path, genome_dir_path, nThreads=8){
+
+  # Type checking.
+  if (typeof(input_dir_path) != "character") {
+    stop(paste("input_dir_path type is", paste0(typeof(input_dir_path), "."), "It should be \"character\""))
+  }
+  if (typeof(genome_dir_path) != "character") {
+    stop(paste("genome_dir_path type is", paste0(typeof(genome_dir_path), "."), "It should be \"character\""))
+  }
+  if (typeof(nThreads) != "double") {
+    stop(paste("nThreads type is", paste0(typeof(nThreads), "."), "It should be \"double\""))
+  }
+
+
+  # Check if input_dir_path exists
+  if (!rrundocker::is_running_in_docker()) {
+    if (!dir.exists(input_dir_path)) {
+      stop(paste("input_dir_path:", input_dir_path, "does not exist."))
+    }
+    if (!dir.exists(genome_dir_path)) {
+      stop(paste("genome_dir_path:", genome_dir_path, "does not exist."))
+    }
+  }
+
+  # Executing the docker job
+  rrundocker::run_in_docker(
+    image_name = paste0("repbioinfo/atacseq:latest"),
+    volumes = list(
+      c(input_dir_path, "/scratch"),
+      c(genome_dir_path, "/genomes"),
+      c(paste0(input_dir_path,"/results"), "/scratch/results")
+    ),
+    additional_arguments = c(
+      "/home/script.sh",
+      nThreads
+    )
+  )
+}
+
+
