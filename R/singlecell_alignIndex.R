7f9cc354c104bbbdb324116acaf52ca1be38909a
agatadonofrio <127829929+agatadonofrio@users.noreply.github.com>
2024-09-09 17:56:38 +0200

    feat(singlecell_alignIndex): add singlecell_alignIndex function
    Adding singlecell_alignIndex.R function. This function has yet to be tested, so there may be the need to fix additional bugs.

---
+++ R/singlecell_alignIndex.R
@@ -0,0 +1,55 @@
+#' Index and Alignment Script for single cell analysis
+#'
+#' This script handles the alignment and indexing of the FASTQ files generated from single-cell RNA-seq experiments using Cell Ranger.
+#' It returns a output directory inside input_dir_path containing aligned BAM files and combined matrix of counts.
+#'
+#' @param input_dir_path, a character string indicating the path of a directory containing the fastq files to be analyzed
+#' @param genome_dir_path, a character string indicating the path of a directory containing the fasta and gtf files of the genome to be analyzed
+#' @param bamsave, a boolean variable indicating if the BAM files are to be saved or not
+#' @author Luca Alessandri, Agata D'Onofrio
+#'
+#' @examples
+#' \dontrun{
+#'  singlecell_alignIndex(
+#'     input_dir_path="/the/input/path",
+#'     genome_dir_path="/the/genome/path",
+#'  )
+#' }
+#' @export
+singlecell_alignIndex <- function(input_dir_path, genome_dir_path, bamsave=TRUE){
+
+  # Type checking.
+  if (typeof(input_dir_path) != "character") {
+    stop(paste("input_dir_path type is", paste0(typeof(input_dir_path), "."), "It should be \"character\""))
+  }
+  if (typeof(genome_dir_path) != "character") {
+    stop(paste("genome_dir_path type is", paste0(typeof(genome_dir_path), "."), "It should be \"character\""))
+  }
+  if (typeof(bamsave) != "boolean") {
+    stop(paste("bamsave type is", paste0(typeof(bamsave), "."), "It should be \"boolean\""))
+  }
+
+  # Check if the paths in input exist
+  if (!rrundocker::is_running_in_docker()) {
+    if (!dir.exists(input_dir_path)) {
+      stop(paste("input_dir_path:", input_dir_path, "does not exist."))
+    }
+    if (!dir.exists(genome_dir_path)) {
+      stop(paste("genome_dir_path:", genome_dir_path, "does not exist."))
+    }
+  }
+
+  # Executing the docker job
+  rrundocker::run_in_docker(
+    image_name = paste0("repbioinfo/carncellranger2:latest"),
+    volumes = list(
+      c(input_dir_path, "/scratch"),
+      c(genome_dir_path, "/genome")
+    ),
+    additional_arguments = c(
+      "/home/index_align.sh",
+      bamsave
+    )
+  )
+}
+
