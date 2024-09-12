a858321e51f1ce10d4b9b026ffaeeee4f9418ad7
agatadonofrio <127829929+agatadonofrio@users.noreply.github.com>
2024-09-12 19:03:09 +0200

    fix: variable type changed from logical to character
    the .sh function inside the docker requires the input to be in a character form over a logical

--- R/singlecell_alignIndex.R
+++ R/singlecell_alignIndex.R
@@ -16,7 +16,7 @@
 #'  )
 #' }
 #' @export
-singlecell_alignIndex <- function(input_dir_path, genome_dir_path, bamsave=TRUE){
+singlecell_alignIndex <- function(input_dir_path, genome_dir_path, bamsave="true"){
 
   # Type checking.
   if (typeof(input_dir_path) != "character") {
@@ -25,8 +25,8 @@ singlecell_alignIndex <- function(input_dir_path, genome_dir_path, bamsave=TRUE)
   if (typeof(genome_dir_path) != "character") {
     stop(paste("genome_dir_path type is", paste0(typeof(genome_dir_path), "."), "It should be \"character\""))
   }
-  if (typeof(bamsave) != "logical") {
-    stop(paste("bamsave type is", paste0(typeof(bamsave), "."), "It should be \"logical\""))
+  if (typeof(bamsave) != "character") {
+    stop(paste("bamsave type is", paste0(typeof(bamsave), "."), "It should be \"character\""))
   }
 
   # Check if the paths in input exist
