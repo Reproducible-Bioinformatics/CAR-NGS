521b04e2fb83ef2dc42b0c2d1d765fd15f35f4ec
Eliseo Martelli <me@eliseomartelli.it>
2024-10-17 16:31:24 +0200

    chore(modules): code formatting

--- R/modules.R
+++ R/modules.R
@@ -1,7 +1,3 @@
-
-modules <- function(input_dir_path, organism, count_matrix, metadata_file, count_sep=NULL, meta_sep=NULL){
-
- # Type checking.
 #' Modules
 #'
 #' The `modules` function runs an RNA-seq data processing pipeline inside a
@@ -37,27 +33,61 @@ modules <- function(input_dir_path, organism, count_matrix, metadata_file, count
 #' }
 #'
 #' @export
+modules <- function(input_dir_path,
+                    organism,
+                    count_matrix,
+                    metadata_file,
+                    count_sep = NULL,
+                    meta_sep = NULL) {
+  # Type checking.
   if (typeof(input_dir_path) != "character") {
-    stop(paste("input_dir_path type is", paste0(typeof(input_dir_path), "."), "It should be \"character\""))
+    stop(
+      paste(
+        "input_dir_path type is",
+        paste0(typeof(input_dir_path), "."),
+        "It should be \"character\""
+      )
+    )
   }
   if (typeof(organism) != "character") {
-    stop(paste("organism type is", paste0(typeof(organism), "."), "It should be \"character\""))
+    stop(
+      paste(
+        "organism type is",
+        paste0(typeof(organism), "."),
+        "It should be \"character\""
+      )
+    )
   }
   if (typeof(count_matrix) != "character") {
-    stop(paste("count_matrix type is", paste0(typeof(count_matrix), "."), "It should be \"character\""))
+    stop(paste(
+      "count_matrix type is",
+      paste0(typeof(count_matrix), "."),
+      "It should be \"character\""
+    ))
   }
   if (typeof(metadata_file) != "character") {
-    stop(paste("metadata_file type is", paste0(typeof(metadata_file), "."), "It should be \"character\""))
+    stop(paste(
+      "metadata_file type is",
+      paste0(typeof(metadata_file), "."),
+      "It should be \"character\""
+    ))
   }
   if (!is.null(count_sep) && typeof(count_sep) != "character") {
-    stop(paste("count_sep type is", paste0(typeof(count_sep), "."),"It should be \"character\""))
+    stop(paste(
+      "count_sep type is",
+      paste0(typeof(count_sep), "."),
+      "It should be \"character\""
+    ))
   }
   if (!is.null(meta_sep) && typeof(meta_sep) != "character") {
-    stop(paste("meta_sep type is", paste0(typeof(meta_sep), "."),"It should be \"character\""))
+    stop(paste(
+      "meta_sep type is",
+      paste0(typeof(meta_sep), "."),
+      "It should be \"character\""
+    ))
   }
 
   # Check if input_dir_path exists
-  if (!is_running_in_docker()) {
   if (!rrundocker::is_running_in_docker()) {
     if (!dir.exists(input_dir_path)) {
       stop(paste("input_dir_path:", input_dir_path, "does not exist."))
@@ -65,11 +95,14 @@ modules <- function(input_dir_path, organism, count_matrix, metadata_file, count
   }
 
   # Setting the separators as NULL
-  if (is.null(count_sep)) { count_sep = "NULL" }
-  if (is.null(meta_sep)) { meta_sep = "NULL" }
+  if (is.null(count_sep)) {
+    count_sep <- "NULL"
+  }
+  if (is.null(meta_sep)) {
+    meta_sep <- "NULL"
+  }
 
   # Executing the docker job
-  run_in_docker(
   rrundocker::run_in_docker(
     image_name = paste0("hedgelab/rnaseq_modules:image5"),
     volumes = list(
